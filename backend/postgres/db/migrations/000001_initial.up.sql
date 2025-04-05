36d8bc0743f5f178dd9c3ac8ffc068603e3440d1
jaybeecave <=>
2025-04-05 13:38:49 +1300

    fix migrations for pg

--- backend/postgres/db/migrations/000001_initial.up.sql
+++ backend/postgres/db/migrations/000001_initial.up.sql
@@ -1,73 +1,67 @@
-DROP TABLE IF EXISTS instances;
-
-CREATE TABLE instances (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  instance_id UUID NOT NULL,
-  execution_id UUID NOT NULL,
-  parent_instance_id UUID NULL,
-  parent_execution_id UUID NULL,
-  parent_schedule_event_id NUMERIC NULL,
-  metadata BYTEA NULL,
-  state INT NOT NULL,
-  created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  completed_at timestamptz NULL,
-  locked_until timestamptz NULL,
-  sticky_until timestamptz NULL,
-  worker VARCHAR(64) NULL
+CREATE TABLE IF NOT EXISTS `instances` (
+  `id` TEXT NOT NULL,
+  `execution_id` TEXT NOT NULL,
+  `parent_instance_id` TEXT NULL,
+  `parent_execution_id` TEXT NULL,
+  `parent_schedule_event_id` INTEGER NULL,
+  `metadata` TEXT NULL,
+  `state` INTEGER NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `completed_at` DATETIME NULL,
+  `locked_until` DATETIME NULL,
+  `sticky_until` DATETIME NULL,
+  `worker` TEXT NULL,
+  PRIMARY KEY(`id`, `execution_id`)
 );
 
-CREATE UNIQUE INDEX idx_instances_instance_id_execution_id on instances (instance_id, execution_id);
-CREATE INDEX idx_instances_locked_until_completed_at on instances (completed_at, locked_until, sticky_until, worker);
-CREATE INDEX idx_instances_parent_instance_id_parent_execution_id ON instances (parent_instance_id, parent_execution_id);
+CREATE INDEX IF NOT EXISTS `idx_instances_id_execution_id` ON `instances` (`id`, `execution_id`);
+CREATE INDEX IF NOT EXISTS `idx_instances_locked_until_completed_at` ON `instances` (`locked_until`, `sticky_until`, `completed_at`, `worker`);
+CREATE INDEX IF NOT EXISTS `idx_instances_parent_instance_id_parent_execution_id` ON `instances` (`parent_instance_id`, `parent_execution_id`);
 
-DROP TABLE IF EXISTS pending_events;
-CREATE TABLE pending_events (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  event_id UUID NOT NULL,
-  sequence_id BIGSERIAL NOT NULL, -- Not used, but keep for now for query compat
-  instance_id UUID NOT NULL,
-  execution_id UUID NOT NULL,
-  event_type INT NOT NULL,
-  timestamp timestamptz NOT NULL,
-  schedule_event_id BIGSERIAL NOT NULL,
-  attributes BYTEA NOT NULL,
-  visible_at timestamptz NULL
+CREATE TABLE IF NOT EXISTS `pending_events` (
+  `id` TEXT,
+  `sequence_id` INTEGER NOT NULL, -- not used but keep for now for query compat
+  `instance_id` TEXT NOT NULL,
+  `execution_id` TEXT NOT NULL,
+  `event_type` INTEGER NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `schedule_event_id` INT NOT NULL,
+  `attributes` BLOB NOT NULL,
+  `visible_at` DATETIME NULL,
+  PRIMARY KEY(`id`, `instance_id`)
 );
 
-CREATE INDEX idx_pending_events_inid_exid ON pending_events (instance_id, execution_id);
-CREATE INDEX idx_pending_events_inid_exid_visible_at_schedule_event_id ON pending_events (instance_id, execution_id, visible_at, schedule_event_id);
+CREATE INDEX IF NOT EXISTS `idx_pending_events_instance_id_execution_id_visible_at_schedule_event_id` ON `pending_events` (`instance_id`, `execution_id`, `visible_at`, `schedule_event_id`);
 
-DROP TABLE IF EXISTS history;
-CREATE TABLE IF NOT EXISTS history (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  event_id UUID NOT NULL,
-  sequence_id BIGSERIAL NOT NULL,
-  instance_id UUID NOT NULL,
-  execution_id UUID NOT NULL,
-  event_type INT NOT NULL,
-  timestamp timestamptz NOT NULL,
-  schedule_event_id BIGSERIAL NOT NULL,
-  attributes BYTEA NOT NULL,
-  visible_at timestamptz NULL
+CREATE TABLE IF NOT EXISTS `history` (
+  `id` TEXT,
+  `sequence_id` INTEGER NOT NULL,
+  `instance_id` TEXT NOT NULL,
+  `execution_id` TEXT NOT NULL,
+  `event_type` INTEGER NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `schedule_event_id` INT NOT NULL,
+  `attributes` BLOB NOT NULL,
+  `visible_at` DATETIME NULL,
+  PRIMARY KEY(`id`, `instance_id`)
 );
 
-CREATE INDEX idx_history_instance_id_execution_id ON history (instance_id, execution_id);
-CREATE INDEX idx_history_instance_id_execution_id_sequence_id ON history (instance_id, execution_id, sequence_id);
+CREATE INDEX IF NOT EXISTS `idx_history_instance_sequence_id` ON `history` (`instance_id`, `execution_id`, `sequence_id`);
 
-DROP TABLE IF EXISTS activities;
-CREATE TABLE IF NOT EXISTS activities (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
-  activity_id UUID NOT NULL,
-  instance_id UUID NOT NULL,
-  execution_id UUID NOT NULL,
-  event_type INT NOT NULL,
-  timestamp timestamptz NOT NULL,
-  schedule_event_id BIGSERIAL NOT NULL,
-  attributes BYTEA NOT NULL,
-  visible_at timestamptz NULL,
-  locked_until timestamptz NULL,
-  worker VARCHAR(64) NULL
+CREATE TABLE IF NOT EXISTS `activities` (
+  `id` TEXT PRIMARY KEY,
+  `instance_id` TEXT NOT NULL,
+  `execution_id` TEXT NOT NULL,
+  `event_type` INTEGER NOT NULL,
+  `timestamp` DATETIME NOT NULL,
+  `schedule_event_id` INT NOT NULL,
+  `attributes` BLOB NOT NULL,
+  `visible_at` DATETIME NULL,
+  `locked_until` DATETIME NULL,
+  `worker` TEXT NULL
 );
 
-CREATE UNIQUE INDEX idx_activities_instance_id_execution_id_activity_id_worker ON activities (instance_id, execution_id, activity_id, worker);
-CREATE INDEX idx_activities_locked_until on activities (locked_until);
+
+CREATE INDEX IF NOT EXISTS `idx_activities_id_worker` ON `activities` (`id`, `worker`);
+CREATE INDEX IF NOT EXISTS `idx_activities_locked_until` ON `activities` (`locked_until`);
+CREATE INDEX IF NOT EXISTS `idx_activities_instance_id_execution_id_worker` ON `activities` (`instance_id`, `execution_id`, `worker`);
